becf7dc5c7e69272163cbe0089c001f985b4bd90
Sylveurdance <sylvain.tiset@gmail.com>
2015-12-01 18:03:10 +0100

    first commit with beginning of db_structure

---
+++ db_php/connect.php
@@ -0,0 +1,41 @@
+<?php
+
+// Usage : require_once("connect.php");
+
+$PARAM_hote='127.0.0.1';
+$PARAM_port='';
+$PARAM_nom_bd='dbWorkManager';
+$PARAM_utilisateur='wmAdmin';
+$PARAM_mot_passe='Y0dBxcA4';
+
+$GLOBALS["BDD_ERROR"] = FALSE;
+
+try {
+	$GLOBALS["BDD_CONNECTION"] = new PDO('pgsql:host='.$PARAM_hote.';port='.$PARAM_port.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur, $PARAM_mot_passe);
+} catch(Exception $e) {
+	echo('Erreur : '.$e->getMessage().'<br />');
+	echo('N° : '.$e->getCode());
+	die();
+}
+
+/*
+Exécute une reqûete SQL
+*/
+function execQuery($query){
+	try {
+		$response=$GLOBALS["BDD_CONNECTION"]->query($query);
+		$err=$GLOBALS["BDD_CONNECTION"]->errorInfo();
+		if (!empty($err[2])) {
+			echo "error <pre>query= ".$query."</pre><br>";
+			echo "<pre>".$err[2]."</pre><br>";
+			$GLOBALS["BDD_ERROR"] = TRUE;
+		}
+		return $response;
+	} catch(Exception $e) {
+		echo('Erreur : <pre>'.$e->getMessage().'</pre><br />');
+		echo('N° : <pre>'.$e->getCode().'</pre>');
+		$GLOBALS["BDD_ERROR"] = TRUE;
+		die();
+	}
+}
+
---
+++ db_sql/create.sql
@@ -0,0 +1,15 @@
+﻿BEGIN;
+
+-- ENUM
+
+CREATE TYPE enum_artist AS ENUM ('Director','Actor','Singer','Writer');
+
+-- TABLE
+
+CREATE TABLE USERS (
+	login VARCHAR(30) PRIMARY KEY,
+	passwd VARCHAR(50) NOT NULL
+);
+
+COMMIT;
+
