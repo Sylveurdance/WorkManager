9d45581fe929fdc2aa92a254c910ce6ea120fcff
Sylveurdance <sylvain.tiset@gmail.com>
2015-12-02 19:02:19 +0100

    write db model

--- db_sql/create.sql
+++ db_sql/create.sql
@@ -3,13 +3,72 @@
 -- ENUM
 
 CREATE TYPE enum_artist AS ENUM ('Director','Actor','Singer','Writer');
+CREATE TYPE enum_work AS ENUM ('Movie','Serie','Book','Song');
+CREATE TYPE enum_genre AS ENUM ('Action','Science-Fiction','Fantastic','Drama','Comedy','Adventure','Spy','Horror','War','Historic','Documentary','Martial-Arts','Western','Thriller','Detective','Theater','Poetry','Pop','PopRock','Rock','HardRock','Metal','Popular','Disco','Electro','Jazz','Funk','Country','Hip-Hop','Rap','Reggeae','Other');
 
 -- TABLE
 
 CREATE TABLE USERS (
-	login VARCHAR(30) PRIMARY KEY,
-	passwd VARCHAR(50) NOT NULL
+	iduser INTEGER AUTO_INCREMENT,
+	login VARCHAR(30) UNIQUE NOT NULL,
+	passwd VARCHAR(50) NOT NULL,
+	PRIMARY KEY(iduser)
 );
+
+CREATE TABLE TYPE (
+	idtype INTEGER AUTO_INCREMENT,
+	genre enum_genre
+	PRIMARY_KEY(idtype)
+);
+
+CREATE TABLE ARTIST (
+	idartist INTEGER AUTO_INCREMENT,
+	name VARCHAR(50),
+	firstname VARCHAR(50),
+	dateofbirth DATE,
+	nationality VARCHAR(30),
+	PRIMARY KEY(idartist)
+);
+
+CREATE TABLE WORK (
+	idwork INTEGER AUTO_INCREMENT,
+	title VARCHAR(50) NOT NULL,
+	releasedate DATE,
+	wlength INTEGER,
+	nationality VARCHAR(50),
+	wtype enum_work,
+	PRIMARY KEY(idwork)
+);
+
+CREATE TABLE ASSOC_USER_WORK (
+	iduser INTEGER NOT NULL,
+	idwork INTEGER NOT NULL,
+	seen BOOLEAN NOT NULL,
+	mark INTEGER CHECK(mark>=0 AND mark<=10),
+	remarks VARCHAR(255),
+	lastupdate DATE
+	PRIMARY KEY(iduser,idwork),
+	FOREIGN KEY(iduser) REFERENCES USERS(iduser),
+	FOEIGN KEY(idwork) REFERENCES WORK(idwork)
+);
+
+CREATE TABLE ASSOC_TYPE_WORK (
+	idwork INTEGER NOT NULL,
+	idtype INTEGER NOT NULL,
+	PRIMARY KEY(idwork,idtype),
+	FOREIGN KEY(idwork) REFERENCES WORK(idwork)
+	FOREIGN KEY(idtype) REFERENCES TYPE(idtype)
+);
+
+CREATE TABLE ASSOC_ARTIST_WORK (
+	idartist INTEGER NOT NULL,
+	idwork INTEGER NOT NULL,
+	job enum_artist,
+	PRIMARY KEY(idartist,idwork),
+	FOREIGN KEY(idartist) REFERENCES ARTIST(idartist),
+	FOEIGN KEY(idwork) REFERENCES WORK(idwork)
+);
+
 
 COMMIT;
 
